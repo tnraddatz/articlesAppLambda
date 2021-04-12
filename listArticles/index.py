127f751d452afeb98b06aac1b560d437f1d02d7b
Thomas Raddatz <tnraddatz@gmail.com>
2021-04-12 14:16:18 -0400

    Updated code

--- listArticles/index.py
+++ listArticles/index.py
@@ -1,17 +1,47 @@
 import json
-import pyjokes
+import boto3
+import html5lib
+from webpreview import web_preview
+from urllib.parse import urlparse
 
 
 def main(event, context):
-    body = {
-        "message": "Go Serverless v1.0! Your function executed successfullys!",
-        "joke": pyjokes.get_joke()
-    }
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(body),
-        "headers": {
-            "Access-Control-Allow-Origin": "*"
+    try:
+        # parameters
+        params = json.loads(event['body'])
+        website_url = params['website_url']
+        # ends
+
+        # Connect to DB and record transaction
+        ddb = boto3.resource('dynamodb')
+        table = ddb.Table('DynamoDevNewsTable')  # TODO: Edit Table Name
+
+        # scan_kwargs = {
+        #     'FilterExpression': Key('year').between(*year_range),
+        #     'ProjectionExpression': "#yr, title, info.rating",
+        #     'ExpressionAttributeNames': {"#yr": "year"}
+        # }
+
+        ddb_response = table.scan()
+        
+        response = {
+            "statusCode": 200,
+            "body": json.dumps(ddb_response['Items']),
+            "headers": {
+                "Access-Control-Allow-Origin": "*"
+            }
         }
-    }
-    return response
+
+        return response
+
+    except Exception as e:
+        response = {
+            "statusCode": 500,
+            "body": json.dumps({
+                "message": str(e)
+            }),
+            "headers": {
+                "Access-Control-Allow-Origin": "*"
+            }
+        }
+        return response
